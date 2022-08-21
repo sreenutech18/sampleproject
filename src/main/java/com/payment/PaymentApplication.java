66d733e50ffc783dce55863f195f161244dd96f2
sreenutech18 <77761730+sreenutech18@users.noreply.github.com>
2022-08-21 13:09:04 +0530

    Update PaymentApplication.java
    updated payment application, added some s.o.p debugging

--- src/main/java/com/payment/PaymentApplication.java
+++ src/main/java/com/payment/PaymentApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PaymentApplication {
 
 	public static void main(String[] args) {
+		
+		System.out.println("*************PaymentApplication**********");
 		SpringApplication.run(PaymentApplication.class, args);
 	}
 
